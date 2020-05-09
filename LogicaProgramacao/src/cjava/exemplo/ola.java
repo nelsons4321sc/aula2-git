9ca547253275a7384bafe500a4418901c9c5bb3a
Nelson Costa <nelsonscosta@yahoo.com.br>
2020-05-09 14:50:20 -0300

    alterado ola.java

--- LogicaProgramacao/src/cjava/exemplo/ola.java
+++ LogicaProgramacao/src/cjava/exemplo/ola.java
@@ -3,8 +3,8 @@ package cjava.exemplo;
 public class ola {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("oi");
+		
+		System.out.println("oi sdsdfsd");
 
 	}
 
